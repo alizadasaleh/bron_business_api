beed7944759aab92666d70c2f9d8da9631dedecb
alizadasaleh <helas2003@gmail.com>
2024-08-17 16:46:36 +0400

    Added company

--- src/main/java/az/bron/business/feature/company/domain/model/Company.java
+++ src/main/java/az/bron/business/feature/company/domain/model/Company.java
@@ -1,5 +1,6 @@
 package az.bron.business.feature.company.domain.model;
 
+import az.bron.business.feature.address.domain.model.Address;
 import az.bron.domain.model.BaseDomain;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -7,4 +8,7 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Company extends BaseDomain<Long> {
+    private String name;
+    private String description;
+    private Address address;
 }
