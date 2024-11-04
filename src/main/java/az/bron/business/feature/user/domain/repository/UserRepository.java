5665abc866a194b492ef4a87d6e7f37d200f3152
alizadasaleh <helas2003@gmail.com>
2024-11-04 23:56:56 +0400

    Errors handled

---
+++ src/main/java/az/bron/business/feature/user/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package az.bron.business.feature.user.domain.repository;
+
+import az.bron.business.feature.company.domain.model.Company;
+import az.bron.business.feature.user.domain.model.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
