f30cfdd0d5ce54e8b98955288bba9a50ff204818
alizadasaleh-dev <saleh.alizada@ailab.az>
2024-11-04 13:15:31 +0400

    Revert "Some changes"
    This reverts commit d391c604be1147d4a07e6bd86d955ba89cd2e7de.

--- src/main/java/az/bron/business/config/S3Config.java
+++ src/main/java/az/bron/business/config/S3Config.java
@@ -8,10 +8,8 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("prod")  // This will only load in the production profile
 public class S3Config {
 
     @Value("${aws.access.key}")
@@ -20,12 +18,16 @@ public class S3Config {
     @Value("${aws.secret.key}")
     private String awsSecretKey;
 
+
     @Bean
     public AmazonS3 s3client() {
+
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
-        return AmazonS3ClientBuilder.standard()
+        var awsS3Config = AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .withRegion(Regions.US_EAST_1)
                 .build();
+
+        return awsS3Config;
     }
 }
