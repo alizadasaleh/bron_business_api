fbe595065a8f189926795df13b83dd7f221224f1
Saleh <saleh.alizada@ailab.az>
2025-01-02 13:09:15 +0400

    BugFix

--- src/main/java/az/bron/business/feature/appointment/application/excpetion/AppointmentNotFound.java
+++ src/main/java/az/bron/business/feature/appointment/application/excpetion/AppointmentNotFound.java
@@ -3,8 +3,8 @@ package az.bron.business.feature.appointment.application.excpetion;
 import az.gov.dlp.exception.NotFoundException;
 import az.gov.dlp.exception.model.ErrorDetails;
 
-public class AppointmentNotFount extends NotFoundException {
-    public AppointmentNotFount(String message) {
+public class AppointmentNotFound extends NotFoundException {
+    public AppointmentNotFound(String message) {
         super(ErrorDetails.of("", "User not found",
                 message,
                 ErrorDetails.error(314_00, "UserNotFound")));
