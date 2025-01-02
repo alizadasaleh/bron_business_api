6725940d020300d92017e8c1b2e5cd7ebd3b8380
Saleh <saleh.alizada@ailab.az>
2025-01-02 13:16:38 +0400

    BugFix

--- init.sql
+++ init.sql
@@ -1,3 +1,15 @@
-CREATE DATABASE bron_business_local;
-CREATE USER bron_user WITH ENCRYPTED PASSWORD 'bron_password';
-GRANT ALL PRIVILEGES ON DATABASE bron_business_local TO bron_user;
+DO $$
+    BEGIN
+        -- Create the database if it doesn't exist
+        IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'bron_business_local') THEN
+            CREATE DATABASE bron_business_local;
+        END IF;
+
+        -- Create the user if it doesn't exist
+        IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'bron_user') THEN
+            CREATE USER bron_user WITH ENCRYPTED PASSWORD 'bron_password';
+        END IF;
+
+        -- Grant privileges
+        GRANT ALL PRIVILEGES ON DATABASE bron_business_local TO bron_user;
+    END $$;
